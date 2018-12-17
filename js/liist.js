c70a431bb89724cdde0bae758f5f33ee7871d3b9
Jack Mandelkorn <jacman444@gmail.com>
2018-12-16 22:52:00 -0800

    Update 1.1
    landing page complete.

--- js/liist.js
+++ js/liist.js
@@ -1,3 +1,11 @@
 Liist.START = () => {
+  Liist.UNLOAD()
   //TODO
+}
+
+Liist.UNLOAD = () => {
+  $(document.body).removeClass("unloaded")
+  $("#loader").fadeOut(1200, () => {
+    $("#loader").remove()
+  })
 }
