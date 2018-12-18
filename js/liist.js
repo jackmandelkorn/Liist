fea6b662ba3b6bafb04fac282a20d0480674f0e3
Jack Mandelkorn <jacman444@gmail.com>
2018-12-17 16:54:23 -0800

    Update 1.3
    layout for the dashboard/app completed

--- js/liist.js
+++ js/liist.js
@@ -2,9 +2,12 @@ Liist.START = () => {
   Liist.SET_PANEL()
   if (Liist.PANEL === "app") {
     document.getElementById("name").innerHTML = Spotify.USER.display_name.toLowerCase().trim()
+    Liist.PLAYLIST_CONTAINER = document.getElementById("playlist-container")
+    Liist.THEME_CONTAINER = document.getElementById("theme-container")
+    Liist.IMAGE_CONTAINER = document.getElementById("image-container")
+    Liist.APP()
   }
   Liist.UNLOAD()
-  //TODO
 }
 
 Liist.UNLOAD = () => {
@@ -27,4 +30,31 @@ Liist.SET_PANEL = (panel) => {
       i--;
     }
   }
+}
+
+Liist.FILL = (color) => {
+  if (!color) {
+    let color = "#19E68C"
+  }
+  let elements = document.getElementsByClassName("color")
+  for (let i = 0; i < elements.length; i++) {
+    let element = elements[i]
+    let className = element.className
+    if (element.className.constructor !== String) {
+      className = element.className.baseVal
+    }
+    if (className.includes("color-color")) {
+      element.style.color = color
+    }
+    else if (className.includes("color-background")) {
+      element.style.background = color
+    }
+    else if (className.includes("color-fill")) {
+      element.style.fill = color
+    }
+  }
+}
+
+Liist.APP = () => {
+  //TODO
 }
